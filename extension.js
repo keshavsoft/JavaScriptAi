2e76ed9263d930a22f31e6cb181098ced48fd25d
keshavsoft <nknnkumar@yahoo.com>
2026-03-24 23:07:02 +0530

    V8 htmlid hooked

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
-import { startOrchestration } from './src/V7/InitJs/orchestration/startOrchestration.js';
-import { startOrchestration as AddListeners } from './src/V7/AddListeners/orchestration/startOrchestration.js';
+import { startOrchestration } from './src/V8/InitJs/orchestration/startOrchestration.js';
+import { startOrchestration as AddListeners } from './src/V8/AddListeners/orchestration/startOrchestration.js';
+import { startOrchestration as AddBusiness } from './src/V8/AddBusiness/orchestration/startOrchestration.js';
 
 export function activate(context) {
 	const disposable = vscode.commands.registerCommand('extension.initJs', (uri) => {
@@ -16,6 +17,14 @@ export function activate(context) {
 	});
 
 	context.subscriptions.push(disposable1);
+
+	const disposable2 = vscode.commands.registerCommand('extension.addBusiness', async (uri) => {
+		const htmlId = await vscode.window.showInputBox({ prompt: 'Enter HTML ID' });
+		if (!htmlId) { return; }
+		AddBusiness({ uri, extensionPath: context.extensionPath, htmlId });
+	});
+
+	context.subscriptions.push(disposable2);
 };
 
 export function deactivate() { };
